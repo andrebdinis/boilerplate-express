b06ea5a69e7692996fa7aeebcf6c7fcc1a69346f
André Barreira Dinis <107752110+andrebdinis@users.noreply.github.com>
2022-06-20 14:39:25 +0000

    Challenges accomplished: 4. Serve Static Assets, 5. Serve JSON on a Specific Route 6. Use the .env File (in this case, on Replit, "Secrets (Environment Variables)"), 7. Implement a Root-Level Request Logger Middleware, 8. Chain Middleware to Create a Time Server, 9. Get Route Parameter Input from the Client, 10. Get Query Parameter Input from the Client, 11. Use body-parser to Parse POST Requests, 12. Get Data from POST Requests.

--- myApp.js
+++ myApp.js
@@ -1,33 +1,66 @@
 let express = require('express');
 let app = express();
+let bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 //console.log("Hello World");
+
 /*app.get("\/", function(req, res) {
   res.send("Hello Express");
 });*/
+
 app.get("\/", function(req, res) {
   const relativePath = "/views/index.html";
   const absolutePath = __dirname + relativePath;
   res.sendFile(absolutePath);
 });
 
+app.use("\/public", express.static(__dirname + "\/public"));
 
+/*app.get("\/json", function(req, res) {
+  res.json({"message": "Hello json"});
+});*/
+// url to see the json message:
+// https://boilerplate-express.andrebdinis.repl.co/json
 
+app.get("\/json", function(req, res) {
+  const MESSAGE_STYLE = process.env.MESSAGE_STYLE;
+  let message = "Hello json";
+  MESSAGE_STYLE==="uppercase" ?
+    res.json({"message": message.toUpperCase()})
+    : res.json({"message": message});
+});
+// url to see the json message:
+// https://boilerplate-express.andrebdinis.repl.co/json
 
+app.get("\/now", function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+},  function(req, res) {
+  res.json({"time": req.time});
+});
 
+app.get("/:word/echo", (req, res) => {
+  res.json({echo: req.params.word});
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// given a query string:
+// /name?first=firstname&last=lastname
+app.route("/name")
+  .get((req, res) => {
+    res.json({ name: `${req.query.first} ${req.query.last}` });
+  })
+  .post((req, res) => {
+    res.json({ name: `${req.body.first} ${req.body.last}` });
+  });
+// url to see the result of a given query string:
+// https://boilerplate-express.andrebdinis.repl.co/name?first=firstname&last=lastname
 
 
 
